29289b976f112a02dc75eacce38e0076427592e3
AdityaSingh <adityasinghhh29@gmail.com>
2024-12-02 12:14:28 +0530

    Initial Commit

---
+++ Team02_SCMTests/Team02_SCMTests.swift
@@ -0,0 +1,17 @@
+//
+//  Team02_SCMTests.swift
+//  Team02_SCMTests
+//
+//  Created by Batch - 2 on 02/12/24.
+//
+
+import Testing
+@testable import Team02_SCM
+
+struct Team02_SCMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
